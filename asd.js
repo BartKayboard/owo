0aaeb9498d8b0b701a966478e1e94eb138967962
Bart <105116958+BartKayboard@users.noreply.github.com>
2023-08-31 19:27:48 -0300

    Add files via upload

---
+++ asd.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const spinButton = document.getElementById('spinButton');
+    const resultSpan = document.getElementById('result');
+    let balance;
+    let winProbabilityPercentage;
+    let loseProbabilityPercentage;
+  
+    // Resto del código...
+    
+  });
